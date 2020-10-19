dd9fcba347f7cf4096d65b204a681c7e6886f234
Avi Warner <aviwarner@gmail.com>
2020-10-19 15:51:49 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,4 +21,5 @@ Array.prototype.forEach.call(elements, function(element) {
   }
 });
 
-console.log('hi friend')
+console.log('%cHi Friend! Watcha looking for?', 
+            'font-weight: bold;font-size: 25px;color: red;text-shadow:3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8), 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
